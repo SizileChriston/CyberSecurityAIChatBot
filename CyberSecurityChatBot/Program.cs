6bf9dc9d4395b2eca1a40a116fe67d132f50df94
SizileChriston <ST10491897@rcconnect.edu.za>
2026-04-12 22:17:26 +0200

    removvede the image that i wanted to display now i want to proceed into displayng the name with an mage added to it, with the right size proportions

--- CyberSecurityChatBot/Program.cs
+++ CyberSecurityChatBot/Program.cs
@@ -20,7 +20,7 @@ namespace CyberSecurityChatBot
 
             Program obj = new Program();
 
-            obj.welcomesound();
+            obj.welcomesound();//this will play the wav sound clip
             obj.asciilogo();//this will end up showing both manual and image ascii, so the logo and image
             obj.getUserDetails();
             obj.wellcomeUser();
@@ -58,36 +58,64 @@ namespace CyberSecurityChatBot
             Console.WriteLine(@"
               
                               
-                               [^_^]
-                               /| |\
-                                / \");
+                                                                       [^_^]
+                                                                       /| |\
+                                                                        / \");
             Console.ForegroundColor = ConsoleColor.Red;
-            Console.WriteLine(@"
-                             
-                     /$$   /$$ /$$                           /$$        /$$$$$$            /$$                                    
-                    | $$  /$$/| $$                          |__/       /$$__  $$          | $$                                    
-                    | $$ /$$/ | $$$$$$$   /$$$$$$   /$$$$$$$ /$$      | $$  \__/ /$$   /$$| $$$$$$$   /$$$$$$   /$$$$$$           
-                    | $$$$$/  | $$__  $$ /$$__  $$ /$$_____/| $$      | $$      | $$  | $$| $$__  $$ /$$__  $$ /$$__  $$          
-                    | $$  $$  | $$  \ $$| $$  \ $$|  $$$$$$ | $$      | $$      | $$  | $$| $$  \ $$| $$$$$$$$| $$  \__/          
-                    | $$\  $$ | $$  | $$| $$  | $$ \____  $$| $$      | $$    $$| $$  | $$| $$  | $$| $$_____/| $$                
-                    | $$ \  $$| $$  | $$|  $$$$$$/ /$$$$$$$/| $$      |  $$$$$$/|  $$$$$$$| $$$$$$$/|  $$$$$$$| $$                
-                    |__/  \__/|__/  |__/ \______/ |_______/ |__/       \______/  \____  $$|_______/  \_______/|__/                
-                                                                                 /$$  | $$                                        
-                                                                                |  $$$$$$/                                        
-                                                                                 \______/                                         
-                      /$$$$$$                                          /$$   /$$                     /$$$$$$$              /$$    
-                     /$$__  $$                                        |__/  | $$                    | $$__  $$            | $$    
-                    | $$  \__/  /$$$$$$   /$$$$$$$ /$$   /$$  /$$$$$$  /$$ /$$$$$$   /$$   /$$      | $$  \ $$  /$$$$$$  /$$$$$$  
-                    |  $$$$$$  /$$__  $$ /$$_____/| $$  | $$ /$$__  $$| $$|_  $$_/  | $$  | $$      | $$$$$$$  /$$__  $$|_  $$_/  
-                     \____  $$| $$$$$$$$| $$      | $$  | $$| $$  \__/| $$  | $$    | $$  | $$      | $$__  $$| $$  \ $$  | $$    
-                     /$$  \ $$| $$_____/| $$      | $$  | $$| $$      | $$  | $$ /$$| $$  | $$      | $$  \ $$| $$  | $$  | $$ /$$
-                    |  $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$/| $$      | $$  |  $$$$/|  $$$$$$$      | $$$$$$$/|  $$$$$$/  |  $$$$/
-                     \______/  \_______/ \_______/ \______/ |__/      |__/   \___/   \____  $$      |_______/  \______/    \___/  
-                                                                                     /$$  | $$                                    
-                                                                                    |  $$$$$$/                                    
-                                                                                     \______/                                     
-                    
-                    ");
+
+            //Console.WriteLine(@"
+
+            //         /$$   /$$ /$$                           /$$        /$$$$$$            /$$                                    
+            //        | $$  /$$/| $$                          |__/       /$$__  $$          | $$                                    
+            //        | $$ /$$/ | $$$$$$$   /$$$$$$   /$$$$$$$ /$$      | $$  \__/ /$$   /$$| $$$$$$$   /$$$$$$   /$$$$$$           
+            //        | $$$$$/  | $$__  $$ /$$__  $$ /$$_____/| $$      | $$      | $$  | $$| $$__  $$ /$$__  $$ /$$__  $$          
+            //        | $$  $$  | $$  \ $$| $$  \ $$|  $$$$$$ | $$      | $$      | $$  | $$| $$  \ $$| $$$$$$$$| $$  \__/          
+            //        | $$\  $$ | $$  | $$| $$  | $$ \____  $$| $$      | $$    $$| $$  | $$| $$  | $$| $$_____/| $$                
+            //        | $$ \  $$| $$  | $$|  $$$$$$/ /$$$$$$$/| $$      |  $$$$$$/|  $$$$$$$| $$$$$$$/|  $$$$$$$| $$                
+            //        |__/  \__/|__/  |__/ \______/ |_______/ |__/       \______/  \____  $$|_______/  \_______/|__/                
+            //                                                                     /$$  | $$                                        
+            //                                                                    |  $$$$$$/                                        
+            //                                                                     \______/                                         
+            //          /$$$$$$                                          /$$   /$$                     /$$$$$$$              /$$    
+            //         /$$__  $$                                        |__/  | $$                    | $$__  $$            | $$    
+            //        | $$  \__/  /$$$$$$   /$$$$$$$ /$$   /$$  /$$$$$$  /$$ /$$$$$$   /$$   /$$      | $$  \ $$  /$$$$$$  /$$$$$$  
+            //        |  $$$$$$  /$$__  $$ /$$_____/| $$  | $$ /$$__  $$| $$|_  $$_/  | $$  | $$      | $$$$$$$  /$$__  $$|_  $$_/  
+            //         \____  $$| $$$$$$$$| $$      | $$  | $$| $$  \__/| $$  | $$    | $$  | $$      | $$__  $$| $$  \ $$  | $$    
+            //         /$$  \ $$| $$_____/| $$      | $$  | $$| $$      | $$  | $$ /$$| $$  | $$      | $$  \ $$| $$  | $$  | $$ /$$
+            //        |  $$$$$$/|  $$$$$$$|  $$$$$$$|  $$$$$$/| $$      | $$  |  $$$$/|  $$$$$$$      | $$$$$$$/|  $$$$$$/  |  $$$$/
+            //         \______/  \_______/ \_______/ \______/ |__/      |__/   \___/   \____  $$      |_______/  \______/    \___/  
+            //                                                                         /$$  | $$                                    
+            //                                                                        |  $$$$$$/                                    
+            //                                                                         \______/                                     
+
+            //        ");
+
+            Console.Write(@"
+
+
+88  dP 88  88  dP""Yb  .dP""Y8 88                                                          
+88odP  88  88 dP   Yb `Ybo."" 88                                                          
+88""Yb  888888 Yb   dP o.`Y8b 88                                                          
+88  Yb 88  88  YbodP  8bodP' 88                                                          
+ dP""""b8 Yb  dP 88""""Yb 888888 88""""Yb                                                      
+dP   `""  YbdP  88__dP 88__   88__dP                                                      
+Yb        8P   88""""Yb 88""""   88""Yb                                                       
+ YboodP  dP    88oodP 888888 88  Yb                                                      
+.dP""Y8 888888  dP""""b8 88   88 88""""Yb 88 888888 Yb  dP      dP""""b8 88  88    db    888888 
+`Ybo."" 88__   dP   `"" 88   88 88__dP 88   88    YbdP      dP   `"" 88  88   dPYb     88   
+o.`Y8b 88""""   Yb      Y8   8P 88""Yb  88   88     8P       Yb      888888  dP__Yb    88   
+8bodP' 888888  YboodP `YbodP' 88  Yb 88   88    dP         YboodP 88  88 dP""""""""Yb   88   
+    88""""Yb  dP""Yb  888888                                                                
+    88__dP dP   Yb   88                                                                  
+    88""""Yb Yb   dP   88                                                                  
+    88oodP  YbodP    88                                                                  
+
+
+
+
+
+
+                                    ");
 
 
             Console.ForegroundColor = ConsoleColor.Green;
@@ -102,44 +130,54 @@ namespace CyberSecurityChatBot
             //create a cyber security themed iiamge
             //ASCII logo
             //now call ascii image
-            asci();//get this ammended image doesnt display as well as sound
+            //asci();//get this ammended image doesnt display as well as sound
+            //removing te png that i wanted to dsplay would ratgher dplay an asci image
         }
 
-        private void asci()
-        {
-            Console.WriteLine("\nLoading Visual Logo");
-            string path = "3pplug.png";//this is where i will put the image file directory
-            //string path = "\"C:\\Users\\Steeze\\OneDrive\\Documents\\School Rosebank\\2026\\Programming 2A\\helloworld\\New folder\\POEChatBot\\POEChatBot\\3pplug.png\"";
+        //private void asci()
+        //{
+        //    Console.WriteLine("\nLoading Visual Logo");
+        //    string path = "3pplug.png";//this is where i will put the image file directory
+        //    //string path = "\"C:\\Users\\Steeze\\OneDrive\\Documents\\School Rosebank\\2026\\Programming 2A\\helloworld\\New folder\\POEChatBot\\POEChatBot\\3pplug.png\"";
+        //    int consoleWidth = Console.WindowWidth;//this will help with resizing the image to fit the console better, also aspect ratio is better now, but still not perfect, gonna try to use a different image and see if it works better
+        //    int consoleHeight = Console.WindowHeight;
 
-            Bitmap image = new Bitmap(path);//the image was located in the wrong path , relocated it and placed it in the debug bin
-                                            //program runs without any errors, but imiage doesnt display properly
-            Bitmap resized = new Bitmap(image, new Size(100, 30));//changed 50 to 30 for better aspect ratio
-                                                                  //resizing the image to fit console better, also aspect ratio is better now, but still not perfect, gonna try to use a different image and see if it works better
+        //    int newWidth = consoleWidth/2;
+        //    int newHeight = consoleHeight/3;  
+
+        //    Bitmap image = new Bitmap(path);//the image was located in the wrong path , relocated it and placed it in the debug bin
+        //                                    //program runs without any errors, but imiage doesnt display properly
+        //    Bitmap resized = new Bitmap(image, new Size(newWidth, newHeight));//changed 50 to 30 for better aspect ratio
+        //                                                          //resizing the image to fit console better, also aspect ratio is better now, but still not perfect, gonna try to use a different image and see if it works better
 
 
-            for (int y = 0; y < resized.Height; y +=2)
-            {
-                for (int x = 0; x < resized.Width; x++)
-                {
-                    Color pixel = resized.GetPixel(x, y);
+        //    for (int y = 0; y < resized.Height; y +=2)
+        //    {
 
-                    int gray = (int)(0.3*pixel.R + 0.59*pixel.G + 0.11*pixel.B) / 3;//image wasnt  being generated properly
-                                                                                    //gonna use better grey scaling
+        //        int padding = (consoleWidth - newWidth) / 2;//this will centre my logo
+        //        Console.Write(new string(' ', padding));
 
-                    string chars = "@#W$9876543210?!abc;:+=-,._ ";//this will provide more levels
-                                                                  // and more darkness 
-                                                                  //still bhaving issues wth dspalying 
+        //        for (int x = 0; x < resized.Width; x++)
+        //        {
+        //            Color pixel = resized.GetPixel(x, y);
 
-                    char asiiChar = chars[gray * chars.Length / 256];
+        //            int gray = (int)(0.3*pixel.R + 0.59*pixel.G + 0.11*pixel.B) / 3;//image wasnt  being generated properly
+        //                                                                            //gonna use better grey scaling
 
-                    Console.Write(asiiChar);
+        //            string chars = "@#W$9876543210?!abc;:+=-,._ ";//this will provide more levels
+        //                                                          // and more darkness 
+        //                                                          //still bhaving issues wth dspalying 
 
-                }
-                Console.WriteLine();//adds line breaks
+        //            char asiiChar = chars[gray * chars.Length / 256];
 
-            }
+        //            Console.Write(asiiChar);
+
+        //        }
+        //        Console.WriteLine();//adds line breaks
+
+        //    }
             
-        }
+        //}
 
         private void getUserDetails()
         {
